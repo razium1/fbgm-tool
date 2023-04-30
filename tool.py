99bb3cb83a7bbc8759fffaa4c7d35488217527a9
BrotherKabeer <105301350+BrotherKabeer@users.noreply.github.com>
2023-04-30 12:38:25 -0500

    Update tool.py

--- tool.py
+++ tool.py
@@ -1,73 +1,116 @@
-import csv
-
-# Create dictionary to store player data
-players = {}
-
-# Open CSV file and read contents
-with open('PlayerRatings.csv', 'r') as file:
-    reader = csv.reader(file)
-    # Skip header row
-    next(reader)
-    # Loop through rows and add data to dictionary
-    for row in reader:
-        name, team, pos, spd, strn, elu, hnd, rtr, rbk, pbk, tck, prs, rns, pcv, thp, tha, thv, pot = row
-        players[name] = {
-            'Team': team,
-            'Pos': pos,
-            'Spd': int(spd),
-            'Str': int(strn),
-            'Elu': int(elu),
-            'Hnd': int(hnd),
-            'RtR': int(rtr),
-            'RBk': int(rbk),
-            'PBk': int(pbk),
-            'Tck': int(tck),
-            'PRs': int(prs),
-            'RnS': int(rns),
-            'PCv': int(pcv),
-            'ThP': int(thp),
-            'ThA': int(tha),
-            'ThV': int(thv),
-            'Pot': int(pot)
-        }
-
-# Function to calculate overall rating for a player
-# Function to calculate overall rating for a player based on position
-def calculate_overall(player):
-    total_weighted = 0
-    pos_weights = {
-        'QB': {'ThP': 3, 'ThA': 3, 'ThV': 3, 'PCv': 2, 'Pot': 4},
-        'RB': {'Spd': 3, 'Str': 3, 'Elu': 3, 'Hnd': 2, 'Pot': 4},
-        'WR': {'Spd': 3, 'RtR': 3, 'Hnd': 3, 'Elu': 2, 'Pot': 4},
-        'TE': {'Spd': 2, 'Str': 3, 'Hnd': 3, 'RtR': 2, 'PBk': 1, 'RBk': 1, 'Pot': 4},
-        'OL': {'Str': 3, 'RBk': 3, 'PBk': 3, 'Pot': 4},
-        'DL': {'Str': 3, 'Tck': 3, 'Elu': 2, 'Pot': 4},
-        'LB': {'Tck': 3, 'Spd': 2, 'Str': 2, 'Elu': 2, 'Pot': 4},
-        'CB': {'Spd': 3, 'RtR': 3, 'Tck': 2, 'Pot': 4},
-        'S': {'Spd': 3, 'Tck': 3, 'PRs': 2, 'Pot': 4},
-        'K': {'ThP': 2, 'KAc': 3, 'KAa': 3, 'Pot': 4},
-        'P': {'KAc': 2, 'KAa': 2, 'KAb': 2, 'Pot': 4}
-    }
-    
-    for attribute, weight in pos_weights[players[player]['Pos']].items():
-        total_weighted += int(players[player][attribute]) * weight
-    
-    return total_weighted // sum(pos_weights[players[player]['Pos']].values())
+import tkinter as tk
+from tkinter import ttk
+from tkinter.filedialog import askopenfilename
+import pandas as pd
 
 
+def create_table(frame):
+    # Create Treeview Frame
+    tree_frame = tk.Frame(frame)
+    tree_frame.pack(side=tk.LEFT, fill=tk.BOTH, expand=1)
+
+    # Create Treeview Scrollbar
+    scrollbar = ttk.Scrollbar(tree_frame)
+    scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
+
+    # Create Treeview
+    tree = ttk.Treeview(tree_frame, yscrollcommand=scrollbar.set)
+    tree.pack(side=tk.LEFT, fill=tk.BOTH, expand=1)
+
+    # Configure Scrollbar
+    scrollbar.config(command=tree.yview)
+
+    # Configure Treeview
+    tree.config(columns=headers, show="headings")
+    for col, header in enumerate(headers):
+        tree.column(col, width=100, anchor="center")
+        tree.heading(col, text=header)
+
+    return tree
+
+
+def update_table(tree, data):
+    # Clear existing data
+    tree.delete(*tree.get_children())
+
+    # Add new data
+    for row in data:
+        tree.insert("", tk.END, values=row)
+
+
+def open_csv_file():
+    # Open file dialog to select CSV file
+    file_path = askopenfilename(filetypes=[("CSV Files", "*.csv")])
+
+    # Read CSV file
+    data = pd.read_csv(file_path)
+
+    # Update Treeview with new data
+    update_table(tree, data.values.tolist())
+
+def search_player():
+    search_value = search_entry.get()
+    if search_value == "":
+        update_table(data)
+    else:
+        filtered_data = []
+        for player in data:
+            if search_value.lower() in player['Name'].lower():
+                filtered_data.append(player)
+        update_table(filtered_data)
 
 
 
-# Function to print player's attributes and overall rating
-def print_player_stats(player):
-    print(f'Player: {player}')
-    for attribute in players[player]:
-        if attribute != 'Team':
-            print(f'{attribute}: {players[player][attribute]}')
-    print(f'Overall rating: {calculate_overall(player)}')
-    print()
+def grade_player(row):
+    # Grade player based on Overall rating
+    if row["Overall"] >= 90:
+        return "A+"
+    elif row["Overall"] >= 80:
+        return "A"
+    elif row["Overall"] >= 70:
+        return "B"
+    elif row["Overall"] >= 60:
+        return "C"
+    else:
+        return "D"
 
-# Test the functions with sample player data
-print_player_stats('Conner Emerson')
-print_player_stats('Hubie Connor')
-print_player_stats('Eric Keeling')
+
+# Create main window
+root = tk.Tk()
+root.title("Football GM Helper")
+
+# Create Frame
+frame = tk.Frame(root)
+frame.pack(fill=tk.BOTH, expand=1)
+
+# Create Treeview Headers
+headers = ["Name", "Team", "Position", "Overall", "Grade"]
+
+# Create Treeview
+tree = create_table(frame)
+
+# Create Open File Button
+open_file_button = tk.Button(root, text="Open CSV File", command=open_csv_file)
+open_file_button.pack(pady=10)
+
+# Create Search Frame
+search_frame = tk.Frame(root)
+search_frame.pack()
+
+# Create Search Label
+search_label = tk.Label(search_frame, text="Search:")
+search_label.pack(side=tk.LEFT)
+
+# Create Search Entry
+search_entry = tk.Entry(search_frame)
+search_entry.pack(side=tk.LEFT, padx=5)
+
+# Create Search Button
+search_button = tk.Button(search_frame, text="Search", command=search_player)
+search_button.pack(side=tk.LEFT)
+
+# Load initial data
+data = pd.read_csv("PlayerRatings.csv")
+update_table(tree, data.values.tolist())
+
+root.mainloop()
